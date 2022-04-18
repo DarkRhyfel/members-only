12744645224137dc36f532ae3c7af34ec95978ae
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-04-18 12:16:48 -0500

    Add authentication
    Add authentication to creating a new post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,13 @@
 
 # Post controller
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
   def index
     @posts = Post.all.order('created_at DESC')
+  end
+
+  def new
+    @post = Post.new
   end
 end
