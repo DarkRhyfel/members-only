86eb95af7f592a2880bb6f4d2672d1936786a3e1
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-04-18 11:33:53 -0500

    Add fields for user
    Add extra fields for user data.

---
+++ db/migrate/20220414224733_add_fields_to_users.rb
@@ -0,0 +1,6 @@
+class AddFieldsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+    add_index :users, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_13_230944) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_224733) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -28,7 +28,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_230944) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
